ad9a3bfc8dfdca53d94749b132bc08d0ad033b75
Wade Cappa <wade.cappa@msn.com>
2025-01-01 15:22:42 -0500

    format

--- taskman/lib/taskman_logic.ex
+++ taskman/lib/taskman_logic.ex
@@ -64,10 +64,12 @@ defmodule Taskman.Logic do
   # TODO: Should return a unique value. This is currently
   #  not enforced at the db level
   defp get_category_id(name, user_id) do
-    category = from(
-      c in Taskman.Categories,
-      where: c.category_name == ^name and c.user_id == ^user_id)
-    |> Taskman.Repo.one()
+    category =
+      from(
+        c in Taskman.Categories,
+        where: c.category_name == ^name and c.user_id == ^user_id
+      )
+      |> Taskman.Repo.one()
 
     case category do
       nil -> :none
@@ -78,8 +80,10 @@ defmodule Taskman.Logic do
   defp get_categories(task_id) do
     from(
       c in Taskman.Categories,
-      join: t in Taskman.TasksToCategories, on: t.category_id == c.category_id,
-      where: t.task_id == ^task_id)
+      join: t in Taskman.TasksToCategories,
+      on: t.category_id == c.category_id,
+      where: t.task_id == ^task_id
+    )
     |> Taskman.Repo.all()
   end
 
@@ -222,9 +226,13 @@ defmodule Taskman.Logic do
       :none ->
         %Taskman.Categories{
           category_name: name,
-          user_id: user_id}
+          user_id: user_id
+        }
         |> Taskman.Repo.insert(returning: true)
-      _id -> {:error, %{reason: "category by this name already exists", user_id: user_id, category_name: name}}
+
+      _id ->
+        {:error,
+         %{reason: "category by this name already exists", user_id: user_id, category_name: name}}
     end
   end
 end
