890530d4bf5f1968fba217c31549eaf6f0481031
Wade Cappa <wade.cappa@msn.com>
2025-02-15 17:25:59 -0500

    better script

--- commands/view_categories.sh
+++ commands/view_categories.sh
@@ -2,9 +2,6 @@
 
 source $HOME/.taskman/config
 
-
-echo "category id, category name, count"
-
 RES=$(curl -sw "%{http_code}" "$TASKURL/category/$1" -H "Authorization: Bearer $TOKEN")
 
 STATUS_CODE=$(echo "$RES" | jq | tail -n1)
@@ -14,5 +11,6 @@ if [ $STATUS_CODE != 200 ] ; then
     echo "status code of $STATUS_CODE"
     echo "$CONTENT" | jq
 else
-    echo "$CONTENT" | jq -r '.[] | [.category_id, .category_name, .count] | @csv'
+    echo "[category id, category name, count]"
+    echo "$CONTENT" | jq -c '.[] | [.category_id, .category_name, .count]'
 fi
