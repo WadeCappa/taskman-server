b1c567444f52277ca72124b4f7c28e2443bf9ebc
Wade Cappa <wade.cappa@msn.com>
2025-02-17 15:26:45 -0500

    better health check scripts

--- upgrade_taskman.sh
+++ upgrade_taskman.sh
@@ -3,7 +3,20 @@
 source $PWD/upgrade_utils.sh
 
 taskman_ready() {
-    docker exec -it $(docker ps -aqf "name=taskman-server-taskman") bash healthcheck.sh
+    SUCCESS_COND="success"
+    FAILURE_COND="failure"
+    CONTAINERS=$(docker ps -aqf "name=taskman-server-taskman")
+    echo "looking at $CONTAINERS"
+    for C in $CONTAINERS; do
+        STATUS=$((docker exec $C bash healthcheck.sh && echo $SUCCESS_COND) || echo $FAILURE_COND)
+        echo "container $C has status of $STATUS"
+        if [ "$STATUS" != $SUCCESS_COND ]; then 
+            echo "failed check for $C"
+            return 1
+        fi
+    done
+    echo "all checks passing"
+    return 0
 }
 
 upgrade_service "taskman" "taskman-server-taskman" taskman_ready
