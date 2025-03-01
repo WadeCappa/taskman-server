2bcb8f3074bbfdae354439437001b01fe20880c2
Wade Cappa <wade.cappa@msn.com>
2025-03-01 12:47:53 -0500

    init

--- authman/lib/authman_application.ex
+++ authman/lib/authman_application.ex
@@ -2,11 +2,8 @@ defmodule Authman.Application do
   def start(_type, _args) do
     children = [
       Authman.Repo,
-      Plug.Cowboy.child_spec(
-        scheme: :http,
-        plug: Authman.Endpoint,
-        options: [port: 4002]
-      )
+      {Plug.Cowboy, scheme: :http, plug: Authman.Endpoint, options: [port: 4002]},
+      {Plug.Cowboy, scheme: :http, plug: Authman.HealthCheck, options: [port: 5501]}
     ]
 
     opts = [strategy: :one_for_one, name: Authman.Supervisor]
--- upgrade_taskman.sh
+++ upgrade_taskman.sh
@@ -2,22 +2,4 @@
 
 source $PWD/upgrade_utils.sh
 
-taskman_ready() {
-    CONTAINER=$1
-
-    SUCCESS_COND="success"
-    FAILURE_COND="failure"
-
-    echo "looking at $CONTAINER"
-    STATUS=$((docker exec $CONTAINER bash healthcheck.sh && echo $SUCCESS_COND) || echo $FAILURE_COND)
-    echo "container $CONTAINER has status of $STATUS"
-    if [ "$STATUS" != $SUCCESS_COND ]; then 
-        echo "failed check for $CONTAINER"
-        return 1
-    fi
-
-    echo "all checks passing for container $CONTAINER"
-    return 0
-}
-
-upgrade_service "taskman" "taskman-server-taskman" taskman_ready
+upgrade_service "taskman" "taskman-server-taskman"
