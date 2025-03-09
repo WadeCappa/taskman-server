5843f4eaec5d7083beb3f418d82788c3d911c84f
Wade Cappa <wade.cappa@msn.com>
2025-03-09 15:06:11 -0400

    Revert "now using env for db connection (#11)"
    This reverts commit a7706a4da57ee4ebb1d827dfe42a27c258b7cb43.

--- taskman/config/config.exs
+++ taskman/config/config.exs
@@ -3,7 +3,7 @@ import Config
 config :taskman, ecto_repos: [Taskman.Repo]
 
 config :taskman, Taskman.Repo,
-  database: System.get_env("DB_TASKMAN_REPO"),
-  username: System.get_env("DB_USERNAME"),
-  password: System.get_env("DB_PASS"),
-  hostname: System.get_env("DB_HOSTNAME")
+  database: "taskman_repo",
+  username: "postgres",
+  password: "pass",
+  hostname: "db"
